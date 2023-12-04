efa2cca26a9c282a2379c09bb669768fd838434d
Avatar Azka <avatar.azka@gmail.com>
2023-12-05 05:48:09 +1000

    build: add tailwind and skeleton

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { invoke } from "@tauri-apps/api/tauri";
+
+	let name = ''
+	let output = ''
+	async function greet() {
+		output = await invoke("greet", { name })
+	}
+</script>
+
+<div class="items-center text-center">
+	<h1>Hello</h1>
+	<div class="items-center text-center">
+		<input class="input" id="greet-input" type="text" placeholder="Enter a name..." bind:value="{name}" />
+		<button class="btn variant-filled-primary" on:click|preventDefault={greet}>Greet</button>
+		{#if output}
+			<p>{output}</p>
+		{/if}
+	</div>
+</div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { purgeCss } from 'vite-plugin-tailwind-purgecss';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig(async () => ({
-  plugins: [svelte()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-  }
-}));
+export default defineConfig({
+	plugins: [sveltekit(), purgeCss()]
+});
